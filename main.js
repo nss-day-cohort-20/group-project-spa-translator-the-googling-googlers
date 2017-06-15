4609942edc70248da410bcafbbe3703c23c184fd
Dustin Walker <thewalkman1234@gmail.com>
2017-06-15 09:00:44 -0500

    russian

---
+++ main.js
@@ -0,0 +1,21 @@
+var English = {
+
+		acquaintance: "aquaintance",
+		
+		pet: "pet",
+
+		familyMember: "",
+
+		soberMessage: "I hope you are not sober",
+
+		choicesMessage: "Make good choices",
+		
+		okMessage: "You are ok",
+
+		cordially: "cordially",
+
+		love: "love",
+		
+		regards: "regards",
+
+	};
